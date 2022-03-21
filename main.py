85432cc5a0ba31918830ae6bdaf28f744815f172
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-03-20 23:00:02 -0600

    Remove saved items

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from argparse import ArgumentParser
+from math import ceil
+from typing import List
 
-from spotipy import Spotify, SpotifyException
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
 from tqdm import tqdm
 
-spotify = Spotify(client_credentials_manager=SpotifyClientCredentials())
+auth_manager = SpotifyOAuth(redirect_uri='localhost:8080')
+cache_token = auth_manager.get_access_token(as_dict=False)
+spotify = Spotify(cache_token)
 
 def get_playlist_items(playlist_id: str):
     response = spotify.playlist_items(playlist_id)
@@ -18,9 +22,9 @@ def get_playlist_items(playlist_id: str):
         items += response['items']
     return items
 
-def get_tracks(playlist_id: str):
+def get_tracks_ids(playlist_id: str):
     items = get_playlist_items(playlist_id)
-    return [item['track'] for item in tqdm(items, 'Getting playlist tracks')]
+    return [item['track']['id'] for item in tqdm(items, 'Getting playlist tracks')]
 
 def get_playlist_id() -> str:
     parser = ArgumentParser(description='spotify-dl allows you to download your spotify songs')
@@ -28,10 +32,22 @@ def get_playlist_id() -> str:
     args = parser.parse_args()
     return args.playlist[0]
 
+def remove_saved(ids: List[str]):
+    chunks = ceil(len(ids) / 50)
+    saved_ids: List[bool] = []
+    for chunk in range(chunks):
+        start = chunk
+        end = chunk + 50
+        response = spotify.current_user_saved_tracks_contains(ids[start:end])
+        saved_ids += [True] * 50 if response is None else response
+    assert len(saved_ids) == len(ids), "There must be the same amount of elements for the filter to work"
+    return [id for idx, id in enumerate(ids) if not saved_ids[idx]]
+
 def main():
     playlist_id = get_playlist_id()
-    tracks = get_tracks(playlist_id)
-    print(playlist_id, tracks)
+    ids = get_tracks_ids(playlist_id)
+    unsaved_ids = remove_saved(ids)
+    print(len(ids), len(unsaved_ids))
 
 if __name__ == "__main__":
     main()
