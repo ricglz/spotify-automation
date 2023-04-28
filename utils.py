b0affe297a3764d85eb2508dbf421626542e45d1
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2023-04-28 01:12:34 -0700

    Make create_chunks to be a generator

--- utils.py
+++ utils.py
@@ -1,18 +1,25 @@
 '''
 Collection of utility functions
 '''
-from math import ceil
-from typing import Iterator, List, TypeVar
+from typing import Iterable, Iterator, List, TypeVar
 
 T = TypeVar('T')
 
-def create_chunks(arr: List[T], chunk_size: int) -> Iterator[List[T]]:
+def create_chunks(iterable: Iterable[T], chunk_size: int) -> Iterator[List[T]]:
     """
     Generate several lists of size `chunk_size` based on the elements in the
     given `arr` list.
     """
-    number_of_chunks = ceil(len(arr) / chunk_size)
-    for chunk in range(number_of_chunks):
-        start = chunk * chunk_size
-        end = start + chunk_size
-        yield arr[start:end]
+    iterator = iter(iterable)
+    while True:
+        try:
+            temp: List[T] = [next(iterator)]
+        except StopIteration:
+            break
+        try:
+            for _ in range(chunk_size - 1):
+                temp.append(next(iterator))
+        except StopIteration:
+            yield temp
+            break
+        yield temp
