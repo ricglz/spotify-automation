cd261f1eb3f4e5f74b22fcf8489161be35b5b5c1
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2024-06-30 12:20:58 -0600

    Optimize to 'cache' the pending playlist elements in a txt file

--- add_music.py
+++ add_music.py
@@ -1,10 +1,10 @@
 from typing import List, Optional, Set, Callable, TypeGuard
-from os import environ
+from os import environ, path, makedirs
 
 from tqdm import tqdm
 
 from spotify import WithId, add_songs_to_playlist, get_saved_ids, \
-                    get_tracks, remove_songs_from_playlist
+    get_tracks, remove_songs_from_playlist
 
 PENDING_PLAYLIST = environ.get('SPOTIFY_PENDING_PLAYLIST', '0')
 
@@ -50,12 +50,36 @@ def remove_saved_songs_of_pending_playlist(ids: List[str]):
     print("Removing saved songs")
     saved_ids = filter_by_saved(ids, get_saved=True)
     remove_songs_from_playlist(saved_ids, PENDING_PLAYLIST)
-    print("Removed saved songs")
+    print("Saved songs removed")
+
+def get_pending_playlist_ids_from_file(filename: str):
+    with open(filename, "r") as file:
+        return file.read().splitlines()
+
+def write_playlist_ids_in_file(ids: List[str], filename: str):
+    makedirs(path.dirname(filename), exist_ok=True)
+    with open(filename, "a+") as file:
+        for id in ids:
+            file.write(f'{id}\n')
+
+def get_pending_playlist_filename(pending_playlist_id: str):
+    return path.expanduser(f"./pending_ids_txt/{pending_playlist_id}.txt")
+
+def get_pending_playlist_ids(pending_playlist_id: str):
+    filename = get_pending_playlist_filename(pending_playlist_id)
+    try:
+        return get_pending_playlist_ids_from_file(filename)
+    except FileNotFoundError:
+        playlist_ids = get_tracks_ids(PENDING_PLAYLIST)
+        write_playlist_ids_in_file(playlist_ids, filename)
+        return playlist_ids
 
 def add_music(collection_id: str, remove_saved: bool):
-    playlist_ids = get_tracks_ids(PENDING_PLAYLIST)
+    playlist_ids = get_pending_playlist_ids(PENDING_PLAYLIST)
     if remove_saved:
         remove_saved_songs_of_pending_playlist(playlist_ids)
     ids = non_duplicate_collection_tracks(collection_id, set(playlist_ids))
     unsaved_ids = filter_by_saved(ids)
     add_songs_to_pending_playlist(unsaved_ids)
+    write_playlist_ids_in_file(
+        unsaved_ids, get_pending_playlist_filename(PENDING_PLAYLIST))
